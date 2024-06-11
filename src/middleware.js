3763a00e5b43301e722f635d303173473554cf6a
Aman <aman30t@gmail.com>
2024-06-12 04:29:33 +0530

    done

--- src/middleware.js
+++ src/middleware.js
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export function middleware(req) {
   const { cookies } = req;
   const token = cookies.get('__Secure-authjs.session-token'); // Assume auth token is stored in cookies
-  
+  console.log(token)
   const url = req.nextUrl.clone();
 
   if (!token) {
